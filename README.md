33fb783264cbb3648fd10c59441b3ff90d01b5a0
Alex <ipritoflex@yandex.ru>
2017-11-08 22:01:39 +0300

    gh-pages

--- README.md
+++
@@ -1,8 +0,0 @@
-# Comments App
-
-```sh
-npm install
-npm start
-```
-
-Then open [http://localhost:8080/](http://localhost:8080/)
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { routerReducer } from "react-router-redux";
-import comments from "./comments";
-
-// main reducers
-export const reducers = combineReducers({
-  routing: routerReducer,
-  comments
-});
